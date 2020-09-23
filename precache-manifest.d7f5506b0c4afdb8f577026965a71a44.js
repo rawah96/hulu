0ae99194f145f49d908c5251f252bd92fa92ae78
rawah <rawah@pdx.edu>
2020-09-22 22:29:18 -0700

    Updates

---
+++ precache-manifest.d7f5506b0c4afdb8f577026965a71a44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "277becfec8b637cfad7952a1f5c23a87",
+    "url": "/hulu/index.html"
+  },
+  {
+    "revision": "51b1a5a3a1c2374b3b8c",
+    "url": "/hulu/static/css/main.fd4f8181.chunk.css"
+  },
+  {
+    "revision": "eb268d232d09f1489cef",
+    "url": "/hulu/static/js/2.8232d0e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hulu/static/js/2.8232d0e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51b1a5a3a1c2374b3b8c",
+    "url": "/hulu/static/js/main.717c65b9.chunk.js"
+  },
+  {
+    "revision": "40ef62de5121f3f1e26d",
+    "url": "/hulu/static/js/runtime-main.01145d28.js"
+  }
+]);
