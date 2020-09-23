15129309cb03f8b1f9381107a0b307cf668fbced
rawah <rawah@pdx.edu>
2020-09-23 00:46:27 -0700

    done with nacv

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header'
+import Nav from './components/Nav/Nav'
+import Results from './components/Results/Results'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Nav />
+      <Results />
     </div>
   );
 }
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Nav.css'
+
+function Nav() {
+    return (
+        <div className="nav">
+            <h5>Trending</h5>
+            <h5>Top Rated</h5>
+            <h5>Action</h5>
+            <h5>Comedy</h5>
+            <h5>Horror</h5>
+            <h5>Romance</h5>
+            <h5>Mystery</h5>
+            <h5>Sci-fi</h5>
+            <h5>Western</h5>
+            <h5>Animation</h5>
+            <h5>TV Movie</h5>
+        </div>
+    )
+}
+
+export default Nav
