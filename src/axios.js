15b2ecf05de9986b2a0aa131f74d4d8807dd4e7c
rawah <rawah@pdx.edu>
2020-12-30 23:04:24 -0800

    added https

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // making an instance of axios so i don't have to include the beggining of the url
 // will append the baseURL to the different requests/fetch urls
 const instance = axios.create({
-    baseURL: "http://api.themoviedb.org/3",
+    baseURL: "https://api.themoviedb.org/3",
 });
 
 export default instance;
